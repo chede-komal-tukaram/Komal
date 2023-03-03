9fe83814ee9b98e869f9a16336ef2e250b478c64
Komal <chedekomal2233@gmail.com>
2023-03-03 15:37:59 +0530

    initial commit

---
+++ Functions.py
@@ -0,0 +1,16 @@
+ #Bacically there are 2 types of function
+#1. Inbuilt Function
+#2. user defined Function
+
+# Inbulilt function/python defined function
+#1. print()
+#2.input()
+#3.range()
+#4. len()
+
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hi")
