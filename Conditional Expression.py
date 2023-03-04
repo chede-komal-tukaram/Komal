de3aaed3af84b75f0f2fc5b5b318133498a91828
Komal <chedekomal2233@gmail.com>
2023-03-04 16:20:51 +0530

    initial commit

---
+++ Conditional Expression.py
@@ -0,0 +1,99 @@
+# a = 10 
+# if (a == 10):
+#     print("equals")
+# else :
+#     print("not equals")
+
+# a = 20
+# if (a == 10):
+#     print("equals")
+# else :
+#     print("not equals")
+
+##------------------------------------------------------------------------------------
+
+# a = 10
+# if a > 10:
+#     print("Greater")
+# if a>= 10 :
+#     print("Greater or equals")
+# if a < 10:
+#     print("lesser")
+# if a <= 10:
+#     print("lesser or equals")           #greater or equals,lesser or equals or equals
+# if a == 10:
+#     print("equals")
+# else:
+#     print("God knows")       
+
+# ##---------------------------------------------------------------------------------------
+
+# a = 10
+# if a > 10:
+#     print("Greater")
+# elif  a >= 10:
+#     print("Greater or equals")
+# elif a < 10:
+#     print("lesser")             #Greater or equals
+# elif a <= 10:
+#     print("lesser or equals")
+# elif a == 10:
+#     print("equals")
+
+##----------------------------------------------------------------------------------------
+# we can have multiple if statement
+#we can have multiple elif statement
+# we can have only one else statement
+
+#we can have single alone if statement
+#we can not have single alone elif statement
+
+# if we combine if with elif or else can we have only one if and else
+#howvever elif can be multiple times.
+
+
+# # # a = 10
+# # # else (a != 10):
+# # # print("equals")
+
+# a = 10 
+# if (a != 10):
+#     print("equals")
+
+# a = 10 
+# elif (a != 10):
+# print("equals")
+
+##---------------------------------------------------------------------------------------------
+#output will be sepend on sequence
+# a = 10
+
+# if a > 10:
+#     print("greater")
+# elif a == 10 :
+#     print("equals")
+# elif a>= 10:
+#     print("greater or equals")
+# elif a < 10:
+#     print("lesser")
+# else:
+#     print("God knows")
+# elif a <= 10:               #syntaxerror: invalid syntax,,we canot write elif statement after else .
+#     print("lesser or equals")
+
+#-------------------------------------------------------------------------------------------------
+# a = 30
+# if a ==10:
+#     print("equals")
+# elif a > 10:
+#     if a > 20:
+#         print("super greater")
+#     else:
+#         print("greater than 10 but lesser than 20")
+# elif a < 10:
+#     if a <5:
+#         print("super lesser")
+#     else:
+#         print("less than 10 but greater than 5")
+
+
