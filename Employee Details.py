3604d97a25f763526e03f506492c17da177f0516
Komal <chedekomal2233@gmail.com>
2023-03-08 14:42:49 +0530

    initial commit

--- Employee Details.py
+++ Employee Details.py
@@ -1,36 +1,36 @@
-class Employee:
-    empid = 1001
-    name = 'komal'
-    address = 'pune'
-    salary = '50000'
+# class Employee:
+#     empid = 1001
+#     name = 'komal'
+#     address = 'pune'
+#     salary = '50000'
 
-    def getInfo(self):
-        print(f"Emp id :{self.empid},name :{self.name},address :{self.address},salary :{self.salary}")
+#     def getInfo(self):
+#         print(f"Emp id :{self.empid},name :{self.name},address :{self.address},salary :{self.salary}")
     
-    def updateInfo(self,empid,name,address):
-        self.empid = empid
-        self.name = name
-        self.address = address
+#     def updateInfo(self,empid,name,address):
+#         self.empid = empid
+#         self.name = name
+#         self.address = address
 
-emp = Employee()
-emp2 =Employee()
+# emp = Employee()
+# emp2 =Employee()
 
 
-emp.getInfo()           #Emp id :1001,name :komal,address :pune,salary :50000
-emp.updateInfo(2002,'kalyani','solapur')
-emp.getInfo()           #Emp id :2002,name :kalyani,address :solapur,salary :50000
+# emp.getInfo()           #Emp id :1001,name :komal,address :pune,salary :50000
+# emp.updateInfo(2002,'kalyani','solapur')
+# emp.getInfo()           #Emp id :2002,name :kalyani,address :solapur,salary :50000
 
-emp2.getInfo()
-emp2.address = 'delhi'
+# emp2.getInfo()
+# emp2.address = 'delhi'
 
-print("Class Level Changes")
-Employee.empid = 3333
-Employee.name = "class level changes"
-Employee.salary = '9999999'
-Employee.address = "UK"
+# print("Class Level Changes")
+# Employee.empid = 3333
+# Employee.name = "class level changes"
+# Employee.salary = '9999999'
+# Employee.address = "UK"
 
-emp.getInfo()       #Emp id :2002,name :kalyani,address :solapur,salary :9999999
-emp2.getInfo()      #Emp id :3333,name :class level changes,address :delhi,salary :9999999 
+# emp.getInfo()       #Emp id :2002,name :kalyani,address :solapur,salary :9999999
+# emp2.getInfo()      #Emp id :3333,name :class level changes,address :delhi,salary :9999999 
 
 
 ###============================================================================================
