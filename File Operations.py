d5fdda5545d458a901d47813de2aab1434670edb
Komal <chedekomal2233@gmail.com>
2023-03-17 15:11:40 +0530

    initial commit

--- File Operations.py
+++ File Operations.py
@@ -35,10 +35,30 @@
 #         filevariable.close()
 #     except:
 #         print("File never opened...")               #file content : 
-# #----------------------------------------------------------------------------------------------
-### another way to open file
+# # #----------------------------------------------------------------------------------------------
+# ### another way to open file
 
-# with
-open (r"D:komal\sample.txt",'r') as filevariable:
-filecontent = filevariable.read()
-print(filecontent)
+# with open (r"D:\Komal\sample.txt",'r') as filevariable:
+#     filecontent = filevariable.read()
+# print(filecontent)
+
+
+#================================================================================================
+#3# file writing operation
+
+# go to file path and locate file with filw name
+#open file name
+#write content in file
+# save file if you made changes (only when you made write something in file)
+#file close
+
+
+# with open(r"D:\Komal\sample.txt",'w') as filevariable:
+#         filevariable.write("we are now expert in python....")
+#         print("file writing is compleated")             #file writing is compleated
+
+
+#=========================================================================================================
+# with open (r"D:\Komal\sample.txt",'r+') as filevariable:
+#         filevariable .write("we are now expert in python...")
+#         print("file writing is compleated....")     #file writing is compleated....
